914c5a83466c4ec12cba12eee22845550a93bf7e
anurag357 <pathakanurag753@gmail.com>
2023-01-31 02:55:36 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import Provider from "./provider";
+import Context from "./context";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Agents = () => {
+  return <AgentOne />
 }
 
-export default App;
+const AgentOne = () => {
+  return <AgentTwo />
+}
+
+
+const AgentTwo = () => {
+  return <AgentBond />
+} 
+
+const AgentBond = () => {
+  return(
+    <Context.Consumer>
+      { context => (
+          <Fragment>
+            <h3>Agent Information</h3>
+            <p>Mission Name: {context.data.name}</p>
+            <p>Mission Agent: {context.data.agent}</p>
+            <p>Mission Accepted: {context.data.accept}</p>
+            <button onClick={context.isMissionAccepted}>Choose Accept</button>
+          </Fragment>
+      )}
+      </Context.Consumer>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Context Api</h1>
+      <Provider>
+        <Agents />
+      </Provider>
+    </div>
+  )
+}
+
+export default App
---
+++ src/problems.js
@@ -0,0 +1,28 @@
+// import React, { useState } from "react";
+
+// const GrondChild = (props) => {
+//   return(
+//     <div>
+//       <h1>Grond Child: </h1>
+//       <Child brond={ props.brond }/>
+//     </div>
+//   )
+// }
+
+// const Child = (props) => {
+//   return (
+//     <h1> Child:{props.brond}</h1>
+//   )
+// }
+
+// function App() {
+//   const [brondname] = useState("Amazon")
+//   return (
+//     <div className="App">
+//       <h1>Hello</h1>
+//       <GrondChild brond={brondname}/>
+//     </div>
+//   );
+// }
+
+// export default App;
